b11bd2bc53b56cd8d432a4a856ede8dc39d87d71
YoungGyuLee <2yg@iyeong-gyuui-MacBook-Pro.local>
2019-01-03 16:56:12 +0900

    Prepare connecting server

--- DowaDog/app/src/main/java/com/takhyungmin/dowadog/community/CommunityFragment.kt
+++ DowaDog/app/src/main/java/com/takhyungmin/dowadog/community/CommunityFragment.kt
@@ -2,8 +2,11 @@ package com.takhyungmin.dowadog.community
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,11 +21,21 @@ import com.takhyungmin.dowadog.communitywrite.CommunityWriteActivity
 import com.takhyungmin.dowadog.presenter.fragment.CommunityFragmentPresenter
 import kotlinx.android.synthetic.main.fragment_community.*
 
+
+
 class CommunityFragment : Fragment() {
 
     lateinit var communityFragmentPresenter : CommunityFragmentPresenter
     lateinit var requestManager: RequestManager
     lateinit var communityAdapter : CommunityAdapter
+    lateinit var communityItems : ArrayList<CommunityItem>
+    var TOTAL_PAGE = 10
+    //서버에서 받아올 수 있나..?
+    var isLoading = false
+    var isLast = false
+    var currentPage = 0
+    var startPage = 0
+    val itemCount = 5
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_community, container, false)
@@ -43,10 +56,44 @@ class CommunityFragment : Fragment() {
     }
 
     fun initView(communityItems : ArrayList<CommunityItem>){
+        this.communityItems = communityItems
         communityAdapter = CommunityAdapter(communityItems, requestManager, communityFragmentPresenter, context!!)
         //contentsEduRvAdapter.setOnItemClickListener(this)
         rv_community_feeds.layoutManager = LinearLayoutManager(activity)
         rv_community_feeds.adapter = communityAdapter
+
+        rv_community_feeds.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                Log.v("isLoading", isLoading.toString())
+                Log.v("isLast", isLast.toString())
+                Log.v("currentPage", currentPage.toString())
+                Log.v("TOTAL_PAGE", TOTAL_PAGE.toString())
+
+                if(!rv_community_feeds.canScrollVertically(-1)) {
+                    // 맨 위
+                }
+                if(!rv_community_feeds.canScrollVertically(1)) {
+                    // 맨 아래
+                    if (!isLoading and !isLast) {
+                        Log.v("continue", "continue")
+                        isLoading = true
+                        currentPage += 1
+                        Log.v("currentPage", currentPage.toString())
+
+                        Handler().postDelayed(Runnable {
+                            communityFragmentPresenter.nextPage(currentPage, itemCount)
+                        }, 2000)
+                    }
+                }
+            }
+
+        })
     }
 
     fun toDetail(){
@@ -57,6 +104,20 @@ class CommunityFragment : Fragment() {
         btn_community_write.clicks().subscribe {
             startActivity(Intent(activity, CommunityWriteActivity::class.java))
         }
+    }
+
+    fun loadNextPage(results : ArrayList<CommunityItem>){
+        communityAdapter.addAll(results)
+        isLoading = false
+        if (currentPage > TOTAL_PAGE)
+            isLast = true
+    }
+
+    fun loadFristPage(results : ArrayList<CommunityItem>){
+
+        communityAdapter.addAll(results)
+
+
     }
 
 }
--- DowaDog/app/src/main/java/com/takhyungmin/dowadog/presenter/fragment/CommunityFragmentPresenter.kt
+++ DowaDog/app/src/main/java/com/takhyungmin/dowadog/presenter/fragment/CommunityFragmentPresenter.kt
@@ -12,13 +12,29 @@ class CommunityFragmentPresenter : BasePresenter<CommunityFragment>() {
     fun initView(){
         communityItems = ArrayList()
 
+        //원래는 통신으로 얻어와야 하지만 일단은..
         communityItems.add(CommunityItem(R.drawable.pic1, "이름1", arrayListOf(R.drawable.pic1), "제목이 블라블라1", "1시간시간"))
         communityItems.add(CommunityItem(R.drawable.pic1, "이름2", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라2", "2시간시간"))
         communityItems.add(CommunityItem(R.drawable.pic1, "이름3", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라3", "3시간시간"))
         communityItems.add(CommunityItem(R.drawable.pic1, "이름4", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라4", "4시간시간"))
         communityItems.add(CommunityItem(R.drawable.pic1, "이름5", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라5", "5시간시간"))
-        communityItems.add(CommunityItem(R.drawable.pic1, "이름6", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라6", "6시간시간"))
-        communityItems.add(CommunityItem(R.drawable.pic1, "이름7", arrayListOf(R.drawable.pic1), "제목이 블라블라7", "7시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름6", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라6", "6시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름7", arrayListOf(R.drawable.pic1), "제목이 블라블라7", "7시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름1", arrayListOf(R.drawable.pic1), "제목이 블라블라1", "1시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름2", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라2", "2시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름3", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라3", "3시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름4", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라4", "4시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름5", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라5", "5시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름6", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라6", "6시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름7", arrayListOf(R.drawable.pic1), "제목이 블라블라7", "7시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름1", arrayListOf(R.drawable.pic1), "제목이 블라블라1", "1시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름2", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라2", "2시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름3", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라3", "3시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름4", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라4", "4시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름5", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라5", "5시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름6", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라6", "6시간시간"))
+//        communityItems.add(CommunityItem(R.drawable.pic1, "이름7", arrayListOf(R.drawable.pic1), "제목이 블라블라7", "7시간시간"))
+
 
 
         view!!.initView(communityItems)
@@ -28,4 +44,18 @@ class CommunityFragmentPresenter : BasePresenter<CommunityFragment>() {
         view!!.toDetail()
     }
 
+
+
+
+    fun nextPage(page : Int, limit : Int){
+        //인자는 통신할 때 쓰일 예정
+        communityItems = ArrayList()
+        communityItems.add(CommunityItem(R.drawable.pic1, "이름6", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라6", "6시간시간"))
+        communityItems.add(CommunityItem(R.drawable.pic1, "이름7", arrayListOf(R.drawable.pic1), "제목이 블라블라7", "7시간시간"))
+        communityItems.add(CommunityItem(R.drawable.pic1, "이름1", arrayListOf(R.drawable.pic1), "제목이 블라블라1", "1시간시간"))
+        communityItems.add(CommunityItem(R.drawable.pic1, "이름2", arrayListOf(R.drawable.pic1, R.drawable.pic1, R.drawable.pic1), "제목이 블라블라2", "2시간시간"))
+        communityItems.add(CommunityItem(R.drawable.pic1, "이름3", arrayListOf(R.drawable.pic1, R.drawable.pic1), "제목이 블라블라3", "3시간시간"))
+
+        view!!.loadNextPage(communityItems)
+    }
 }
